5ebe28c24450b0635889787e18c219b8cc384679
JB Sinluenam <jb.sinluenam@gmail.com>
2023-05-22 20:30:43 -0700

    AuthForm

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,7 @@
+//Create fetcher function to make API calls
+//This function will be used by SWR to fetch data from the API
+//In this function, we pass the object with the URL, method, body, and json parameters
+
 const fetcher = async ({ url, method, body, json = true }) => {
 
   const res = await fetch(url, {
@@ -9,16 +13,21 @@ const fetcher = async ({ url, method, body, json = true }) => {
     },
   });
 
+  //Throw an error if the response is not ok
   if (!res.ok) {
     throw new Error('Network response was not ok');
   }
 
+  //If the json parameter is true, then we parse the response as JSON and return the data
   if (json) {
     const data = await res.json();
     return data;
   }
 };
 
+//Create register function to make a POST request to the /api/register endpoint
+//This function will be used by the register page to register a new user
+//We pass the user object which contains the name, email, and password
 export const register = async (user) => {
   return fetcher({
     url: '/api/register',
@@ -28,9 +37,9 @@ export const register = async (user) => {
   });
 };
 
-export const login = async (user) => {
+export const signin = async (user) => {
   return fetcher({
-    url: '/api/login',
+    url: '/api/signin',
     method: 'POST',
     body: user,
     json: false,
